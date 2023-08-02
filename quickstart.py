3a56d5e14e97c931796fde4757fe541cd5b6c7f1
08Miguel24 <MichaelAveuc571@gmail.com>
2023-08-02 17:00:09 +0800

    update

--- quickstart.py
+++ quickstart.py
@@ -1,56 +1,115 @@
-from __future__ import print_function
+import os
+import sys
+import requests
+import smtplib
+from email.message import EmailMessage
+from argparse import ArgumentParser
 
-import os.path
+from typing import List
+from dotenv import load_dotenv
+from pathlib import Path
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-
-
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secrets.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
-
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+import pandas as pd
 
 
-if __name__ == '__main__':
-    main()
+def load_env_file() -> None:
+    BASE_DIR = Path('./').resolve()
+    load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+def load_company_list(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path, index_col=0)
+    return df
+
+def load_files(path: str, is_txt=False) -> tuple:
+    with open(path, 'rb') as file:
+        # read binary data of file. If file is .txt then deocde to normal string
+        file_data = file.read() if is_txt is False else file.read().decode('ascii')
+
+        # get base name of file before sending
+        file_name = os.path.basename(file.name)
+
+    return file_data, file_name
+
+def create_emails(company_list: pd.DataFrame, position: str='Data Analytics', subject='Data Analytics application') -> List[EmailMessage]:
+    # initialize list to contain all email objects
+    # messages = []
+
+    # load cover letter and resume
+    resume, resume_name = load_files('./documents/Cueva, Larry Miguel_Resume_A.pdf')
+    cover_letter, _ = load_files('./documents/DA_cover_letter_2.txt', is_txt=True)
+
+    def helper(row):
+        company_name = row['company_name']
+        email = row['email']
+        # print(cover_letter.format(position='Data Analytics', company_name=company_name))
+
+        # create email objects. Add file also
+        # as attachment to email object
+        msg = EmailMessage()
+        msg['Subject'] = subject
+        msg['To'] = email
+        msg.set_content(cover_letter.format(position=position, company_name=company_name))
+        msg.add_attachment(resume, maintype='application', subtype='octet-stream', filename=resume_name)
+        
+        return email
+    
+    messages = company_list.apply(helper, axis=1)
+
+    # # extract company emails
+    # for row in company_list.index:
+    #     data = company_list.loc[row, :]
+    #     print(data)
+
+    #     company_name = data['company_name']
+    #     email = data['email']
+    #     print(cover_letter.format(position='Data Analytics', company_name=company_name))
+
+    #     # create email objects. Add file also
+    #     # as attachment to email object
+    #     msg = EmailMessage()
+    #     msg['Subject'] = 'test'
+    #     msg['To'] = email
+    #     msg.set_content(cover_letter.format(position='Data Analytics', company_name=company_name))
+    #     msg.add_attachment(resume, maintype='application', subtype='octet-stream', filename=resume_name)
+
+    #     # append msg object to messages list
+    #     messages.append(msg)
+    
+    return messages
+
+if __name__ == "__main__":
+    # port argument and server
+    parser = ArgumentParser()
+    parser.add_argument('--host', type=str, default='smtp.gmail.com')
+    parser.add_argument('--port', type=int, default='465')
+
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+    
+    # load path to .env file
+    load_env_file()
+
+    # extract env variables
+    SENDER_EMAIL = os.environ['SENDER_EMAIL']
+    SENDER_PASSWORD = os.environ['SENDER_PASSWORD']
+
+    # load csv of company meta data
+    company_list = load_company_list('./documents/dummy.csv')
+    print(company_list.head())
+
+    messages = create_emails(company_list)
+
+
+    # # this block will send the email message to either a local 
+    # # host or the email address it was intended to go
+    # if host == "localhost" or host == "127.0.0.1":
+    #     with smtplib.SMTP(host, port) as smtp:
+    #         # send message to local host at port 1025
+    #         smtp.send_message(msg=msg)
+    #         smtp.send
+    # else:
+    #     with smtplib.SMTP_SSL(host, port) as smtp:
+    #         # login with credentials
+    #         smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
+    #         smtp.send_message(msg=msg)
--- readme.md
+++ readme.md
@@ -2,36 +2,13 @@
 
 # A mini-project that automates sending my cv/resume and cover letter to companies and job recruiters en masse.
 
-# Note: Gmail API requires OAuth2.0 for authentication. PyDrive makes your life much easier by handling complex authentication steps for you.**
+# Replication:
+1. generate app password on your google account: https://myaccount.google.com/apppasswords
+2. to use it open the *Mail* app.
+3. Open the *Settings* menu.
+4. Select *Accounts* and then select your Google Account.
+5. Replace your password with the 16-character password shown above.
+6. Just like your normal password, this app password grants complete access to your Google Account. You won't need to remember it, so don't write it down or share it with
 
-# Go to APIs Console and make your own project.
-1. Search for **Gmail API**
-2. select the entry, and click **Enable**.
-3. Select **Credentials** from the left menu, click **Create Credentials**, select **OAuth client ID**.
-4. Now, the product name and consent screen need to be set -> click **Configure consent screen** and follow the instructions.
-5. Once finished select **Application type** to be *Web application*.
-6. Enter an appropriate name.
-7. Input *http://localhost:8080* for **Authorized JavaScript origins**.
-8. Input *http://localhost:8080/* for **Authorized redirect URIs**.
-9. Click **Save**.
-10. Click **Download JSON** on the right side of Client ID to download `client_secret_<really long ID>.json`.
-11. The downloaded file has all authentication information of your application. Rename the file to `client_secrets.json` and place it in your working directory.
-
-# if an error 403 occurs fix this issue by the following steps
-1. Go to again https://console.developers.google.com/
-2. On the top left beside the words "Google APIs" click the project dropdown on the right
-3. Ensure that your correct project is selected
-4. Click "OAuth consent screen" on the left side of the screen (below "Credentials")
-5. If you have not created a consent screen, do that first
-6. Under "Test users" there is a button called "+ ADD USERS"
-7. Type the email of the account you will be testing with, press enter, then click save.
-8. It should work now!
-
-# This message [(403) Access Not Configured]
-...means that when you set up your Google Drive Access, you missed out "Activate the google drive API". You did not configure the Drive API to enabled in your Google account. To Activate the Drive API, go to Developers Console and Enable the Google Drive API.
-
-Here's a link to enable Drive API: https://developers.google.com/drive/v3/web/enable-sdk
-
-# 400 error
-1. Assuming we have placed in our Authorized redirect URIs https://developers.google.com/oauthplayground go to that link
-2. ![oauthplayground](./assets/mediafiles/image.png)
+# Implemenation ideas:
+1. maybe instead of loop use multithreading or concurrent execution to deliver emails at faster speeds
