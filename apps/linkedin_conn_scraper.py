f577c2a4d0286b611c52cb88de7031df966fd77d
08Miguel24 <MichaelAveuc571@gmail.com>
2023-12-18 19:00:37 +0800

    u[pdat

--- apps/linkedin_conn_scraper.py
+++ apps/linkedin_conn_scraper.py
@@ -50,7 +50,7 @@ def collect_recruiter_info(driver: webdriver.Chrome, links: list[str]):
         finally:
             print("will go to next link")
 
-def element_exists(driver: webdriver.Chrome, css_path: str, ):
+def element_exists(driver: webdriver.Chrome, css_path: str,):
     """
     should .find_element() raise a NoSuchElement or StaleElementReference
     exception return false because the element being searched does not exist
@@ -62,12 +62,23 @@ def element_exists(driver: webdriver.Chrome, css_path: str, ):
     return True
 
 
-def click_until_permitted(driver: webdriver.Chrome):
+def click_until_permitted(driver: webdriver.Chrome, css_path: str, num_tries: int=50):
     """
     clicks an element over and over if it is stale
-    """
 
-    pass
+    if click is successful function will return true 
+    """
+    while num_tries > 0:
+        try:
+            """THIS DOESN"T WORK BECAUSE EVEN IF THE ELEMTN EXISTS THE FACT THAT IT IS DYNAMIC
+            WILL MAKES IT STALE AND THEREFORE CLICKING ON IT WILL RAISE A STALE ELEMENT REFERENCE
+            ERROR"""
+            load_more_btn = driver.find_element(By.CSS_SELECTOR, css_path)
+            load_more_btn.click()
+            return True
+        except (StaleElementReferenceException, NoSuchElementException) as e:
+            print(f'error {e} has occured')
+            num_tries -= 1
 
 
 def extract_con_links(driver: webdriver.Chrome | webdriver.Edge, lookup_file: pd.DataFrame):
@@ -83,6 +94,8 @@ def extract_con_links(driver: webdriver.Chrome | webdriver.Edge, lookup_file: pd
     # new or existing dataframe lookup_file
     conn_links = []
     conn_names = []
+
+    click_permitted = False
 
     try:
         # login first
@@ -136,17 +149,8 @@ def extract_con_links(driver: webdriver.Chrome | webdriver.Edge, lookup_file: pd
             # sometimes infinite scrollling will stop and instead show 
             # load more button. If this is the case check if such an 
             # element exists and just click on it then continue scrolling down
-            if element_exists(driver=driver, css_path=".scaffold-layout__main .scaffold-finite-scroll__load-button") == True:
-                print("element exists")
-                driver.refresh()
-                time.sleep(5)
+            click_until_permitted(driver=driver, css_path=".scaffold-layout__main .scaffold-finite-scroll__load-button")
                 
-                """THIS DOESN"T WORK BECAUSE EVEN IF THE ELEMTN EXISTS THE FACT THAT IT IS DYNAMIC
-                WILL MAKES IT STALE AND THEREFORE CLICKING ON IT WILL RAISE A STALE ELEMENT REFERENCE
-                ERROR"""
-                load_more_btn = driver.find_element(By.CSS_SELECTOR, ".scaffold-layout__main .scaffold-finite-scroll__load-button")
-                load_more_btn.click()
-                time.sleep(1.5)
         
         connections = driver.find_elements(By.CSS_SELECTOR, ".artdeco-list")
         # print("test", connections)
